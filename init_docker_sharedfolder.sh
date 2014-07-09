1d3b01fcfe4b4ff2cb7b6e2afb4ddee129591cfa
Akira Osada <osd.akira@gmail.com>
2014-07-09 22:26:53 +0900

    一旦コミット

--- init_docker_sharedfolder.sh
+++ init_docker_sharedfolder.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+
+set -x
+
 VM_NAME=boot2docker-vm-vboxfs
 WORKSPACE=`pwd`
+
+if [ $(boot2docker --vm=boot2docker-vm-vboxfs status) != "running" ]; then
+    boot2docker --vm=boot2docker-vm-vboxfs up
+fi
 
 VBoxManage sharedfolder \
     add "$VM_NAME" \
@@ -10,3 +17,5 @@ VBoxManage sharedfolder \
 
 boot2docker --vm=$VM_NAME ssh \
     "sudo modprobe vboxsf && sudo mkdir -p $WORKSPACE && sudo mount -t vboxsf $( basename $WORKSPACE ) $WORKSPACE"
+
+set +x
